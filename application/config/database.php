2bd34bdc5856cd7e6d2ce2b33bb7eaa6b546bb4b
King Villanueva <parvillanueva@gmail.com>
2019-11-26 11:18:55 +0800

    udpate database

--- application/config/database.php
+++ application/config/database.php
@@ -2,33 +2,32 @@
 
 		$config["installed"] = "yes";
 
-		$whitelist = array(
-		    "127.0.0.1",
-		    "::1"
-		);
+		$root = $_SERVER['HTTP_HOST'];
+		switch ($root) {
+			case 'decolgen.unilab.com.ph':
+				$db_host = "";
+				$db_username = "";
+				$db_password = "";
+				$db_database = "";
+				break;
 
-		if(in_array($_SERVER["REMOTE_ADDR"], $whitelist)){
-		    $db_host = "172.29.70.125";
-			$db_username = "phpdevsite";
-			$db_password = "unilab123";
-			$db_database = "decolgen";
-		}else{
-			$url = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
-			if (strpos($url,"webqa.unilab.com.ph") !== false) {
-				/* WEBQA Credentials */
-			    $db_host = preg_replace("(^https?://)", "", base_url() );
+			case 'decolgen.webqa.unilab.com.ph':
+				$db_host = "";
 				$db_username = "";
 				$db_password = "";
 				$db_database = "";
-			} else {
-				/* PRODUCTION Credentials */
-			    $db_host = preg_replace("(^https?://)", "", base_url() );
-				$db_username = "";
-				$db_password = "";
-				$db_database = "";
-			}
+				break;
+			
+			default:
+				$db_host = "172.29.70.125";
+				$db_username = "phpdevsite";
+				$db_password = "unilab123";
+				$db_database = "decolgen";
+				break;
 		}
 
+		
+		
 		$active_group = "default";
 		$query_builder = TRUE;
 		$db["default"] = array(
