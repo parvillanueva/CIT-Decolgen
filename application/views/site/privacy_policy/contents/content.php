6f26acf7aa44ab1450ffd679e5ee34031b1aa376
Jan <janjalanie@hotmail.com>
2019-11-26 16:37:20 +0800

    added Privacy Policy page

---
+++ application/views/site/privacy_policy/contents/content.php
@@ -0,0 +1,19 @@
+    <div class="dc-main-content dc-fade-blue dc-noise">
+        <div class="dc-section" id="asc">
+            <div class="dc-container">
+                <div class="text-center">
+                    <span class="dc-h3">Privacy Policy</span>
+                    <p class="dc-main-desc" style="text-align: justify;">
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                      <br><br>
+                        Why do we use it?
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php $this->load->view("site/home/footer"); ?>
