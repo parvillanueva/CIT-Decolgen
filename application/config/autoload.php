4eec5b3a72d76a1edd3e7b243c0c774622ccf4ff
Jan <janjalanie@hotmail.com>
2019-11-26 11:03:44 +0800

    Fix Database

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -1,8 +1,8 @@
 <?php defined("BASEPATH") OR exit("No direct script access allowed");
 		$autoload["packages"] = array();
-		$autoload["libraries"] = array("form_validation", "session","standard","minify","user_agent","load_form");
+		$autoload["libraries"] = array("database", "form_validation", "session","standard","minify","user_agent","display","social","send","form_table","themes");
 		$autoload["drivers"] = array();
-		$autoload["helper"] = array("url","html","file");
+		$autoload["helper"] = array("url","html","file","captcha");
 		$autoload["config"] = array();
 		$autoload["language"] = array();
 		$autoload["model"] = array("content_management/Global_model","content_management/Custom_model");
